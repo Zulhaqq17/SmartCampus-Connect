2e7d9608910c735ccda51a3b91866604d7bbe05b
rahimannn <rahimanroslan42@gmail.com>
2026-07-08 18:43:49 +0800

    Update EnrolmentServiceApplicationTests.java

--- campus-enrolment-service/enrolment-service/src/test/java/my/utem/bitp3123/enrolment_service/EnrolmentServiceApplicationTests.java
+++ campus-enrolment-service/enrolment-service/src/test/java/my/utem/bitp3123/enrolment_service/EnrolmentServiceApplicationTests.java
@@ -1,13 +1,1 @@
-package my.utem.bitp3123.enrolment_service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EnrolmentServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
