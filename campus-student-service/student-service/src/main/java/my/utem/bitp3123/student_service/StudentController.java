5b28c03ef8b1e80161f97dfd5f39670bd90d76bd
rahimanroslan42-glitch <rahimanroslan42@gmail.com>
2026-07-08 14:08:31 +0800

    Update StudentController.java

--- campus-student-service/student-service/src/main/java/my/utem/bitp3123/student_service/StudentController.java
+++ campus-student-service/student-service/src/main/java/my/utem/bitp3123/student_service/StudentController.java
@@ -1,44 +1,4 @@
+
 package my.utem.bitp3123.student_service;
 
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
-import java.util.*;
 
-@RestController // Expose endpoints as a REST web service
-@RequestMapping("/api/students") // This is the base URL path
-public class StudentController {
-	
-	private final StudentService service;
-	
-	// Constructor
-	public StudentController(StudentService service) {
-		this.service = service;
-	}
-	
-	// GET /api/students to retrieve all profiles
-	@GetMapping
-	public List<Student> getAll(){
-		return service.findAll();
-	}
-	
-	// GET /api/students/{id} to get individyal profile
-	@GetMapping("/{id}")
-	public ResponseEntity<Student> getOne(@PathVariable int id){
-		return service.findById(id)
-				.map(ResponseEntity::ok) // return 200 ok with object data
-				.orElse(ResponseEntity.notFound().build()); // return 400 not found
-	}
-	
-	// POST /api/students - Create new student profile
-	@PostMapping
-	public ResponseEntity<Student> create(@RequestBody Student student) {
-		
-		// input validation
-		if (student.getName() == null || student.getName().isBlank()) {
-			return ResponseEntity.badRequest().build(); // return 400 bad request
-		}
-		Student created = service.create(student);
-		return ResponseEntity.status(HttpStatus.CREATED).body(created); // return 201 created
-	}
-
-}
