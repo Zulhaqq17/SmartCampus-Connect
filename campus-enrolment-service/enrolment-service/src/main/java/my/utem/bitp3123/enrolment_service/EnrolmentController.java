96d232792ae3eb72112e670ea09dbb55fcbb5fc8
rahimannn <rahimanroslan42@gmail.com>
2026-07-08 18:45:05 +0800

    Update EnrolmentController.java

--- campus-enrolment-service/enrolment-service/src/main/java/my/utem/bitp3123/enrolment_service/EnrolmentController.java
+++ campus-enrolment-service/enrolment-service/src/main/java/my/utem/bitp3123/enrolment_service/EnrolmentController.java
@@ -4,34 +4,3 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
-
-@RestController
-@RequestMapping("/api/enrolments")
-public class EnrolmentController {
-
-	private final EnrolmentService service;
-	
-	public EnrolmentController(EnrolmentService service) {
-		this.service = service;
-	}
-	
-	// GET /api/enrolments to view all enrolments records
-	@GetMapping
-	public List<Enrolment> getAll(){
-		return service.findAll();
-	}
-	
-	@PostMapping
-	public ResponseEntity<Enrolment> create(@RequestBody Enrolment request) {
-		
-		System.out.println("Received Student ID: " + request.getStudentId());
-		System.out.println("Received Course Code: " + request.getCourseCode());
-		if (request.getStudentId() <= 0 || request.getCourseCode() == null) {
-			return ResponseEntity.badRequest().build();
-		}
-		
-		Enrolment result = service.enrollStudent(request.getStudentId(), request.getCourseCode());
-		return ResponseEntity.status(HttpStatus.CREATED).body(result);
-	}
-	
-}
